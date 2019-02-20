e270c57f472f2d12b53d921974ae4604a1cf6d07
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-21 00:53:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,42 @@
 <?php
+// urutkan config nya kaya contoh dibawah akun ke 2/3 jg sama !!
+
+// config
 $config = [
-"imei" => "865759031177962",
-"sign" => "3f47dc72081c5cc66d47552ac57f76433e32004e",
-"token" => "710b7532cb6d769f5c598ceeef601c1d",
-"uuid" => "0bb4d815-9e13-464e-99b3-265c6079e558"
+"imei" => [
+// akun ke 1
+"865759031177962",
+// akun ke 2
+"345677899087532",
+// akun ke 3
+"456567654321906",
+],
+  "sign" => [
+// akun ke 1
+"850713c0497431e67cb7e7adf9039cd629a9c976",
+// akun ke 2
+"58c98dd1863e80eb0d4711d29427ae2f0ae951b1",
+// akun ke 3
+"6d3a86402ef7d9126b44ab7636018fe4d8cbc3de",
+],
+  "token" => [
+// akun ke 1
+"6e23c47356048b24ef0e58d30444fa33",
+// akun ke 2
+"04ba6df97dd4e9b647c013299bfc4eb5",
+// akun ke 3
+"69f51e12e3bf00fcfa664e1c3efc7c5d",
+],
+  "uuid" => [
+// akun ke 1
+"00ee7e0f-31cb-47b5-bcf8-f1cfbfd548e7",
+// akun ke 2
+"f53086a8jsp9j4k7",
+// akun ke 3
+"u75864v6pyv7p0q5",
+],
+
 ];
+
+
+?>
